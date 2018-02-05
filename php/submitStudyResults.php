02bd097873a1bb47c25d9f168d8ee24774802cf5
meqdad.darweesh <meqdad.darweesh@gmail.com>
2018-02-05 13:11:06 +0200

    Storing JSON on server.

--- php/submitStudyResults.php
+++ php/submitStudyResults.php
@@ -2,16 +2,13 @@
 header("Access-Control-Allow-Origin: *");
 $conn = new mysqli('****', '****', '****', '****');
 
-echo "hi\n";
 if(!isset($_POST['clientid'], $_POST['data']))
-	die();
-echo "hi2\n\";
+	die('');
+
 $clientid=$_POST['clientid'];
 $data=$_POST['data'];
-echo $_POST['clientid'] . "\n";
-$clientid = mysqli_real_escape_string($con, $clientid);
-$data = mysqli_real_escape_string($con, $data);
+$clientid = mysqli_real_escape_string($conn, $clientid);
+$data = mysqli_real_escape_string($conn, $data);
 
 $sql = "INSERT INTO studyresults(clientid,data) VALUES ('$clientid','$data')";
 $result = $conn->query($sql);
-echo '<pre>', print_r($result), '</pre>', "\n";
